38b02c859b0d2391ea4a1c32d90573918e535276
kaien <kaienmili@126.com>
2015-12-01 10:13:40 +0800

    打一次

---
+++ aaa/aaa/ViewController.m
@@ -0,0 +1,28 @@
+//
+//  ViewController.m
+//  aaa
+//
+//  Created by tarena13 on 15/12/1.
+//  Copyright (c) 2015年 tarena. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    NSLog(@"sdfs");
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
